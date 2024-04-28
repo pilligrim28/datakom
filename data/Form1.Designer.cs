81442f31746d9404c1668f351af7a53c1dea0c1b
Александр Николаев <alexandrnikolaev1@gmail.com>
2024-04-28 23:06:11 +0300

    Добавьте файлы проекта.

---
+++ data/Form1.Designer.cs
@@ -0,0 +1,355 @@
+﻿namespace data
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dateTimePicker1 = new System.Windows.Forms.DateTimePicker();
+            this.dateTimePicker2 = new System.Windows.Forms.DateTimePicker();
+            this.label1 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.radioAir = new System.Windows.Forms.RadioButton();
+            this.radioRails = new System.Windows.Forms.RadioButton();
+            this.radioBus = new System.Windows.Forms.RadioButton();
+            this.panZone = new System.Windows.Forms.Panel();
+            this.maskedTextBox2 = new System.Windows.Forms.MaskedTextBox();
+            this.maskedTextBox1 = new System.Windows.Forms.MaskedTextBox();
+            this.dateTimePicker3 = new System.Windows.Forms.DateTimePicker();
+            this.dateTimePicker4 = new System.Windows.Forms.DateTimePicker();
+            this.transport = new System.Windows.Forms.Panel();
+            this.panel3 = new System.Windows.Forms.Panel();
+            this.radioRus = new System.Windows.Forms.RadioButton();
+            this.radioBugor = new System.Windows.Forms.RadioButton();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.panZone.SuspendLayout();
+            this.transport.SuspendLayout();
+            this.panel3.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // dateTimePicker1
+            // 
+            this.dateTimePicker1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.dateTimePicker1.Location = new System.Drawing.Point(29, 60);
+            this.dateTimePicker1.Name = "dateTimePicker1";
+            this.dateTimePicker1.Size = new System.Drawing.Size(169, 26);
+            this.dateTimePicker1.TabIndex = 0;
+            // 
+            // dateTimePicker2
+            // 
+            this.dateTimePicker2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.dateTimePicker2.Location = new System.Drawing.Point(29, 134);
+            this.dateTimePicker2.Name = "dateTimePicker2";
+            this.dateTimePicker2.Size = new System.Drawing.Size(169, 26);
+            this.dateTimePicker2.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label1.Location = new System.Drawing.Point(289, 60);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(19, 20);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "1";
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.button1.Location = new System.Drawing.Point(29, 401);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(107, 37);
+            this.button1.TabIndex = 3;
+            this.button1.Text = "Рассчет";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label2.Location = new System.Drawing.Point(25, 37);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(181, 20);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Начало командировки";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label3.Location = new System.Drawing.Point(25, 111);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(169, 20);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "Конец командировки";
+            // 
+            // radioAir
+            // 
+            this.radioAir.AutoSize = true;
+            this.radioAir.Location = new System.Drawing.Point(18, 23);
+            this.radioAir.Name = "radioAir";
+            this.radioAir.Size = new System.Drawing.Size(95, 24);
+            this.radioAir.TabIndex = 10;
+            this.radioAir.TabStop = true;
+            this.radioAir.Text = "Самолёт";
+            this.radioAir.UseVisualStyleBackColor = true;
+            this.radioAir.CheckedChanged += new System.EventHandler(this.radioAir_CheckedChanged);
+            // 
+            // radioRails
+            // 
+            this.radioRails.AutoSize = true;
+            this.radioRails.Location = new System.Drawing.Point(18, 47);
+            this.radioRails.Name = "radioRails";
+            this.radioRails.Size = new System.Drawing.Size(76, 24);
+            this.radioRails.TabIndex = 11;
+            this.radioRails.TabStop = true;
+            this.radioRails.Text = "Поезд";
+            this.radioRails.UseVisualStyleBackColor = true;
+            this.radioRails.CheckedChanged += new System.EventHandler(this.radioRails_CheckedChanged);
+            // 
+            // radioBus
+            // 
+            this.radioBus.AutoSize = true;
+            this.radioBus.Location = new System.Drawing.Point(18, 71);
+            this.radioBus.Name = "radioBus";
+            this.radioBus.Size = new System.Drawing.Size(89, 24);
+            this.radioBus.TabIndex = 12;
+            this.radioBus.TabStop = true;
+            this.radioBus.Text = "Автобус";
+            this.radioBus.UseVisualStyleBackColor = true;
+            this.radioBus.CheckedChanged += new System.EventHandler(this.radioBus_CheckedChanged);
+            // 
+            // panZone
+            // 
+            this.panZone.Controls.Add(this.label6);
+            this.panZone.Controls.Add(this.label5);
+            this.panZone.Controls.Add(this.dateTimePicker4);
+            this.panZone.Controls.Add(this.dateTimePicker3);
+            this.panZone.Controls.Add(this.maskedTextBox2);
+            this.panZone.Controls.Add(this.maskedTextBox1);
+            this.panZone.Location = new System.Drawing.Point(267, 172);
+            this.panZone.Name = "panZone";
+            this.panZone.Size = new System.Drawing.Size(285, 223);
+            this.panZone.TabIndex = 13;
+            this.panZone.Visible = false;
+            // 
+            // maskedTextBox2
+            // 
+            this.maskedTextBox2.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.maskedTextBox2.Location = new System.Drawing.Point(20, 136);
+            this.maskedTextBox2.Mask = "00:00";
+            this.maskedTextBox2.Name = "maskedTextBox2";
+            this.maskedTextBox2.Size = new System.Drawing.Size(67, 32);
+            this.maskedTextBox2.TabIndex = 11;
+            this.maskedTextBox2.ValidatingType = typeof(System.DateTime);
+            // 
+            // maskedTextBox1
+            // 
+            this.maskedTextBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.maskedTextBox1.Location = new System.Drawing.Point(20, 45);
+            this.maskedTextBox1.Mask = "00:00";
+            this.maskedTextBox1.Name = "maskedTextBox1";
+            this.maskedTextBox1.Size = new System.Drawing.Size(67, 32);
+            this.maskedTextBox1.TabIndex = 10;
+            this.maskedTextBox1.ValidatingType = typeof(System.DateTime);
+            // 
+            // dateTimePicker3
+            // 
+            this.dateTimePicker3.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.dateTimePicker3.Location = new System.Drawing.Point(93, 45);
+            this.dateTimePicker3.Name = "dateTimePicker3";
+            this.dateTimePicker3.Size = new System.Drawing.Size(169, 26);
+            this.dateTimePicker3.TabIndex = 12;
+            // 
+            // dateTimePicker4
+            // 
+            this.dateTimePicker4.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.dateTimePicker4.Location = new System.Drawing.Point(93, 136);
+            this.dateTimePicker4.Name = "dateTimePicker4";
+            this.dateTimePicker4.Size = new System.Drawing.Size(169, 26);
+            this.dateTimePicker4.TabIndex = 13;
+            // 
+            // transport
+            // 
+            this.transport.Controls.Add(this.label7);
+            this.transport.Controls.Add(this.radioRails);
+            this.transport.Controls.Add(this.radioAir);
+            this.transport.Controls.Add(this.radioBus);
+            this.transport.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.transport.Location = new System.Drawing.Point(29, 278);
+            this.transport.Name = "transport";
+            this.transport.Size = new System.Drawing.Size(200, 117);
+            this.transport.TabIndex = 14;
+            this.transport.Visible = false;
+            // 
+            // panel3
+            // 
+            this.panel3.Controls.Add(this.label4);
+            this.panel3.Controls.Add(this.radioBugor);
+            this.panel3.Controls.Add(this.radioRus);
+            this.panel3.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.panel3.Location = new System.Drawing.Point(29, 172);
+            this.panel3.Name = "panel3";
+            this.panel3.Size = new System.Drawing.Size(200, 100);
+            this.panel3.TabIndex = 15;
+            // 
+            // radioRus
+            // 
+            this.radioRus.AutoSize = true;
+            this.radioRus.Location = new System.Drawing.Point(18, 33);
+            this.radioRus.Name = "radioRus";
+            this.radioRus.Size = new System.Drawing.Size(80, 24);
+            this.radioRus.TabIndex = 0;
+            this.radioRus.TabStop = true;
+            this.radioRus.Text = "Россия";
+            this.radioRus.UseVisualStyleBackColor = true;
+            this.radioRus.CheckedChanged += new System.EventHandler(this.radioRus_CheckedChanged);
+            // 
+            // radioBugor
+            // 
+            this.radioBugor.AutoSize = true;
+            this.radioBugor.Location = new System.Drawing.Point(18, 63);
+            this.radioBugor.Name = "radioBugor";
+            this.radioBugor.Size = new System.Drawing.Size(119, 24);
+            this.radioBugor.TabIndex = 1;
+            this.radioBugor.TabStop = true;
+            this.radioBugor.Text = "Зарубежная";
+            this.radioBugor.UseVisualStyleBackColor = true;
+            this.radioBugor.CheckedChanged += new System.EventHandler(this.radioBugor_CheckedChanged);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label4.Location = new System.Drawing.Point(27, 4);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(121, 20);
+            this.label4.TabIndex = 2;
+            this.label4.Text = "Командировка";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label5.Location = new System.Drawing.Point(20, 10);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(214, 20);
+            this.label5.TabIndex = 14;
+            this.label5.Text = "Приграничная зона Россия";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label6.Location = new System.Drawing.Point(23, 106);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(190, 20);
+            this.label6.TabIndex = 15;
+            this.label6.Text = "1-ый Зарубежный город";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label7.Location = new System.Drawing.Point(27, 0);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(133, 20);
+            this.label7.TabIndex = 13;
+            this.label7.Text = "Вид транспорта";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label8.Location = new System.Drawing.Point(289, 94);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(19, 20);
+            this.label8.TabIndex = 16;
+            this.label8.Text = "1";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(602, 450);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.panel3);
+            this.Controls.Add(this.transport);
+            this.Controls.Add(this.panZone);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.dateTimePicker2);
+            this.Controls.Add(this.dateTimePicker1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.panZone.ResumeLayout(false);
+            this.panZone.PerformLayout();
+            this.transport.ResumeLayout(false);
+            this.transport.PerformLayout();
+            this.panel3.ResumeLayout(false);
+            this.panel3.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DateTimePicker dateTimePicker1;
+        private System.Windows.Forms.DateTimePicker dateTimePicker2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.RadioButton radioAir;
+        private System.Windows.Forms.RadioButton radioRails;
+        private System.Windows.Forms.RadioButton radioBus;
+        private System.Windows.Forms.Panel panZone;
+        private System.Windows.Forms.DateTimePicker dateTimePicker4;
+        private System.Windows.Forms.DateTimePicker dateTimePicker3;
+        private System.Windows.Forms.MaskedTextBox maskedTextBox2;
+        private System.Windows.Forms.MaskedTextBox maskedTextBox1;
+        private System.Windows.Forms.Panel transport;
+        private System.Windows.Forms.Panel panel3;
+        private System.Windows.Forms.RadioButton radioBugor;
+        private System.Windows.Forms.RadioButton radioRus;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label8;
+    }
+}
+
---
+++ data/Form1.cs
@@ -0,0 +1,112 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace data
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+           
+            DateTime date1 = DateTime.Parse(dateTimePicker1.Text);
+            DateTime date2 = DateTime.Parse(dateTimePicker2.Text);
+            int days = 1+(date2-date1).Days;
+            label1.Text = days.ToString();
+            if (radioRus.Checked)
+            {
+                int summ = days * 700;
+                label8.Text = summ.ToString();
+            }
+            if (radioBugor.Checked)
+            {
+                
+                if (radioAir.Checked)
+                {
+                    if (days == 1)
+                    {
+                        int summ = days * 1000;
+                        label8.Text = summ.ToString();
+                    }
+                    else
+                    {
+                        int summ = ((days - 1) * 1000) + 700;
+                        label8.Text = summ.ToString();
+                    }
+                }
+                if(radioBus.Checked || radioRails.Checked)
+                {
+                    string time1 = maskedTextBox1.Text;
+                    string time2 = maskedTextBox2.Text;
+                    DateTime t1 = DateTime.ParseExact(time1, "HH:mm", CultureInfo.InvariantCulture);
+                    DateTime t2 = DateTime.ParseExact(time2, "HH:mm", CultureInfo.InvariantCulture);
+                    DateTime date3 = DateTime.Parse(dateTimePicker3.Text);
+                    DateTime date4 = DateTime.Parse(dateTimePicker4.Text);
+                    int days1 = date3.Day;
+                    int days2 = date4.Day;
+                    if (days1!=days2)
+                    {
+                        if (days == 1)
+                        {
+                            int summ = days * 1000;
+                            label8.Text = summ.ToString();
+                        }
+                        else
+                        {
+                            int summ = ((days - 2) * 1000) + (700 * 2);
+                            label8.Text = summ.ToString();
+                        }
+                    }
+                     else if(t1 < t2 || days1 <= days2)
+                    {
+                        MessageBox.Show("Проверьте 1-й Зарубежный город");
+                    }
+                    else
+                    {
+                        int summ = ((days - 1) * 1000) + 700;
+                        label8.Text = summ.ToString();
+                    }
+                }
+
+            }
+        }
+
+        private void radioBugor_CheckedChanged(object sender, EventArgs e)
+        {
+            transport.Visible = true;
+        }
+
+        private void radioRus_CheckedChanged(object sender, EventArgs e)
+        {
+            transport.Visible = false;
+            panZone.Visible = false;
+        }
+
+        private void radioAir_CheckedChanged(object sender, EventArgs e)
+        {
+            panZone.Visible = false;
+        }
+
+        private void radioRails_CheckedChanged(object sender, EventArgs e)
+        {
+            panZone.Visible = true;
+        }
+
+        private void radioBus_CheckedChanged(object sender, EventArgs e)
+        {
+            panZone.Visible = true;
+        }
+    }
+}
